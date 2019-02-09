481f5a67b77d5cbb31b5b27af24d1f58fbaf45ac
diohabara <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2019-02-09 23:53:06 +0900

    2/9 added: minpro2019d

---
+++ Atcoder/others/minpro2019d.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int l, a[200000];
+    cin >> l;
+    rep(i, l) { cin >> a[i]; }
+    
+    return 0;
+}
