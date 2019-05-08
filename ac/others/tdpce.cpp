5e909a65d70421b41db6ca4c77e9aaa9bf14fca3
diohabara <diohabara@gmail.com>
2019-05-08 15:33:32 +0900

    5/8

--- ac/others/tdpce.cpp
+++ ac/others/tdpce.cpp
@@ -6,41 +6,54 @@ using namespace std;
 int dy[4] = { 1, 0, -1, 0 }, dx[4] = { 0, 1, 0, -1 };
 typedef pair<ll, ll> P;
 const int MOD = 1e9 + 7;
-int a[20000];
-int N, D;
+int D;
+string N;
+// dp[pos][sum][sml]
+// pos:pos桁目まで見たときの
+// sum:sum=(各桁の和 mod D)
+// sml:パスに制限があるかどうかsml->ある・sml=1->ない
+int dp[10001][101][2];
 
-int dp[2000][2][100];
-int calc(int digit, int sml, int sum)
+int solve()
 {
-    // 桁は
-    if (digit == N) {
-        return (sum == 0);
-    }
-    if (~dp[digit][sml][sum]) {
-        return dp[digit][sml][sum];
-    }
-    int ret = 0;
-    for (int i = 0; i < 10; i++) {
-        if (sml == 0 && a[digit] < i) {
-            break;
+    const int len = N.size();
+    dp[0][0][0] = 1;
+
+    for (int pos = 0; pos < len; pos++) {
+        for (int sum = 0; sum < D; sum++) {
+            for (int sml = 0; sml <= 1; sml++) {
+                if (!dp[pos][sum][sml]) {
+                    //ここまでのパスが存在しない場合
+                    continue;
+                }
+                if (sml == 1) {
+                    // 次へのパスに制限がない場合
+                    int lim = 10;
+                    for (int d = 0; d < lim; d++) {
+                        (dp[pos + 1][(sum + d) % D][1] += dp[pos][sum][1]) %= MOD;
+                    }
+                } else {
+                    // 次へのパスに制限がある場合
+                    int lim = static_cast<int>(N[pos] - '0');
+                    for (int d = 0; d < lim; d++) {
+                        (dp[pos + 1][(sum + d) % D][1] += dp[pos][sum][0]) %= MOD;
+                    }
+                    (dp[pos + 1][(sum + lim) % D][0] += dp[pos][sum][0]) %= MOD;
+                }
+            }
         }
-        int val;
-        val = calc(digit + 1, sml | (a[digit] != i), (sum % i) % D);
-        ret = (val + ret) % MOD;
     }
-    return dp[digit][sml][sum] = ret;
+
+    // Dの倍数で制限のある場合とない場合の和が答え
+    int res = (dp[len][0][0] + dp[len][0][1]) % MOD;
+    return (res + MOD - 1) % MOD;
 }
 int main()
 {
     cin.tie(0);
     ios::sync_with_stdio(false);
-    string s;
-    cin >> D >> s;
-    N = s.size();
-    for (int i = 0; i < N; i++) {
-        a[i] = s[i] - '0';
-    }
-    fill_n(**dp, 20000 * 2 * 100, -1);
-    cout << (calc(0, 0, 0) + MOD - 1) % MOD << endl;
+
+    cin >> D >> N;
+    cout << solve() << endl;
     return 0;
 }
