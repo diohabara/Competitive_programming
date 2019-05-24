228c54384cbb5ccee0e54cec9e2298fbcad8cc75
diohabara <diohabara@gmail.com>
2019-05-24 21:34:35 +0900

    5/24

--- ac/abc/017c.cpp
+++ ac/abc/017c.cpp
@@ -2,12 +2,33 @@
 #define ll long long
 #define endl '\n'
 using namespace std;
-int dy[4] = {1,0,-1,0},dx[4] = {0,1,0,-1};
+ll dy[4] = { 1, 0, -1, 0 }, dx[4] = { 0, 1, 0, -1 };
 typedef pair<ll, ll> P;
+ll N, M;
 
-int main() {
+int main()
+{
     cin.tie(0);
     ios::sync_with_stdio(false);
-    
+    cin >> N >> M;
+    ll l, r, s;
+    vector<ll> arr(M + 1);
+    ll ans = 0;
+    for (ll i = 0; i < N; i++) {
+        cin >> l >> r >> s;
+        l--;
+        arr[l] += s;
+        arr[r] -= s;
+        ans += s;
+    }
+
+    ll min_n = 1e9;
+    for (ll i = 0; i < M; i++) {
+        arr[i + 1] += arr[i];
+    }
+    for (ll i = 0; i < M; i++) {
+        min_n = min(min_n, arr[i]);
+    }
+    cout << ans - min_n << endl;
     return 0;
 }
