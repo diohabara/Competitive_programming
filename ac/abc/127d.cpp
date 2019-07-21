3448a40e00db000d3039fcee71bab14a54b84b68
diohabara <diohabara@gmail.com>
2019-07-21 03:58:55 -0700

    7/21

--- ac/abc/127d.cpp
+++ ac/abc/127d.cpp
@@ -1,24 +1,37 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define endl '\n'
 using namespace std;
-int dy[4] = { 1, 0, -1, 0 }, dx[4] = { 0, 1, 0, -1 };
-typedef pair<ll, ll> P;
-
+#define ll long long
+#define rep(i, n) for (ll i = 0; i < n; ++i)
+#define ALL(a) a.begin(), a.end()
+ll dx[4] = { 0, 1, 0, -1 };
+ll dy[4] = { 1, 0, -1, 0 };
+const ll INF = 1e14;
+const ll MOD = 1e9 + 7;
 int main()
 {
+    ios_base::sync_with_stdio(false);
     cin.tie(0);
-    ios::sync_with_stdio(false);
-    ll n, m;
-    cin >> n >> m;
-    vector<ll> a(n);
-    vector<P> bc(m);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
+    // input
+    ll N, M;
+    cin >> N >> M;
+    vector<ll> a(N);
+    vector<pair<ll, ll>> cb(M);
+    rep(i, N) { cin >> a[i]; }
+    rep(i, M) { cin >> cb[i].second >> cb[i].first; }
+    // output
+    sort(a.begin(), a.end());
+    sort(cb.rbegin(), cb.rend());
+    int i = 0, j = 0;
+    while (i < N && j < M) {
+        ll tmp = 0;
+        while (a[i] < cb[j].first && tmp < cb[j].second) {
+            a[i] = cb[j].first;
+            i++;
+            tmp++;
+        }
+        j++;
     }
-    for (int i = 0; i < m; i++) {
-        cin >> bc[i].first >> bc[i].second;
-    }
-    
+    ll ans = accumulate(a.begin(), a.end(), 0ll);
+    cout << ans << endl;
     return 0;
 }
