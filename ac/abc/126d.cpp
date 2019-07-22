ee311f55e1b033d86ac11d6783ae16d904affb43
diohabara <diohabara@gmail.com>
2019-07-22 03:06:04 -0700

    7/22

--- ac/abc/126d.cpp
+++ ac/abc/126d.cpp
@@ -1,14 +1,43 @@
 #include <bits/stdc++.h>
 #define rep(i, n) for (int i = 0; i < n; i++)
 #define ll long long
-#define endl '\n'
 using namespace std;
-int dy[4] = {1,0,-1,0},dx[4] = {0,1,0,-1};
+int dy[4] = { 1, 0, -1, 0 }, dx[4] = { 0, 1, 0, -1 };
 typedef pair<ll, ll> P;
+int main()
+{
+    int N;
+    cin >> N;
+    vector<int> G[N], cost[N];
+    for (int i = 0; i < N - 1; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        u--, v--;
+        G[u].push_back(v);
+        G[v].push_back(u);
+        cost[u].push_back(w);
+        cost[v].push_back(w);
+    }
 
-int main() {
-    cin.tie(0);
-    ios::sync_with_stdio(false);
+    vector<int> res(N, -1);
+    res[0] = 0;
+    stack<int> s;
+    s.push(0);
+    while (!s.empty()) {
+        int v = s.top();
+        s.pop();
+        for (int i = 0; i < G[v].size(); i++) {
+            int u = G[v][i];
+            int w = cost[v][i];
 
+            if (res[u] != -1)
+                continue;
+            res[u] = (res[v] + w) % 2;
+            s.push(u);
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+        cout << res[i] << endl;
     return 0;
 }
