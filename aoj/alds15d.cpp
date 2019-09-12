fc46295c3fdb4526dcf7aa2b511d0ab81012ecb9
diohabara <diohabara@gmail.com>
2019-09-12 19:13:40 +0900

    alds15d

--- aoj/alds15d.cpp
+++ aoj/alds15d.cpp
@@ -7,14 +7,66 @@ ll dx[4] = { 0, 1, 0, -1 };
 ll dy[4] = { 1, 0, -1, 0 };
 const ll INF = 1e14;
 const ll MOD = 1e9 + 7;
+
+#define MAX 200000
+#define SENTINEL 2000000000
+
+ll L[MAX / 2 + 2], R[MAX / 2 + 2];
+
+ll merge(vector<ll> A, ll left, ll mid, ll right)
+{
+    ll i, j;
+    ll cnt = 0;
+    ll n1 = mid - left;
+    ll n2 = right - mid;
+    for (i = 0; i < n1; i++) {
+        L[i] = A[left + i];
+    }
+    for (i = 0; i < n2; i++) {
+        R[i] = A[mid + i];
+    }
+    L[n1] = SENTINEL;
+    R[n2] = SENTINEL;
+    i = 0;
+    j = 0;
+    for (int k = left; k < right; k++) {
+        if (L[i] <= R[j]) {
+            A[k] = L[i];
+            i++;
+        } else {
+            A[k] = R[j];
+            j++;
+            cnt += (n1 - i);
+        }
+    }
+    return cnt;
+}
+
+ll mergeSort(vector<ll> A, ll left, ll right)
+{
+    ll v1, v2, v3;
+    if (left + 1 < right) {
+        ll mid = (left + right) / 2;
+        v1 = mergeSort(A, left, mid);
+        v2 = mergeSort(A, mid, right);
+        v3 = merge(A, left, mid, right);
+        cout << v1 + v2 + v3 << endl;
+        return v1 + v2 + v3;
+    } else {
+        return 0;
+    }
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    int n;
+    ll n;
     cin >> n;
-    vector<int> a(n);
+    vector<ll> a(MAX);
     rep(i, n) { cin >> a[i]; }
-    
+
+    ll ans = mergeSort(a, 0, n);
+    cout << ans << endl;
     return 0;
 }
