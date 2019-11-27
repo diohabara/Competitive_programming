ffe992e0e93915c4b7e17cb3e3c8b47963adbb0a
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2019-11-27 15:06:01 +0900

    11/27

--- aoj/alds114c.cpp
+++ aoj/alds114c.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef unsigned long long ull;
-#define rep(i, n) for (int i = 0; i < (int)n; i++)
-ull A = 9999973; // 縦
-ull B = 950527; // 横
+#define rep(i, n) for (int i = 0; i < n; i++)
+ull A = 9999973;
+ull B = 950527;
 
 int H, W, R, C;
-string txt[1005]; // 検索テキスト
-string pat[1005]; // 検索パターン
+string txt[1005];
+string pat[1005];
 ull t[1005][1005];
 ull t2[1005][1005];
 
@@ -24,46 +24,47 @@ int main()
         cin >> pat[i];
     }
 
-    // patternのハッシュ値を決めている
     ull target = 0;
-    rep(i, R) {
+    rep(i, R)
+    {
         ull key = 0;
-        rep(j, C) {
+        rep(j, C)
+        {
             key = key * B + pat[i][j];
         }
         target = target * A + key;
     }
 
-    ull c = pow(B, C);
+    ull c = 1;
+    rep(i, C) c *= B;
 
-    // textのハッシュ配列を決めている
-    rep(i, H) {
+    rep(i, H)
+    {
         ull key = 0;
-        rep(j, W) {
+        rep(j, W)
+        {
             key = key * B + txt[i][j];
-            if (j - C >= 0) { // これよりはpatternが収まらない
+            if (j - C >= 0)
                 key -= txt[i][j - C] * c;
-            }
             t[i][j] = key;
             t2[i][j] = t[i][j];
         }
     }
 
-    c = pow(A, R);
+    c = 1;
+    rep(i, R) c *= A;
 
-    rep(i, H) {
-        rep(j, W) {
-            if (i) { // 最初の行以外
-                t[i][j] += t[i - 1][j] * A; // 前の行の値を加える
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            if (i) {
+                t[i][j] += t[i - 1][j] * A;
             }
-            if (i - R >= 0) { //ここからはpatternが収まらない
+            if (i - R >= 0) {
                 t[i][j] -= t2[i - R][j] * c;
             }
-            if (t[i][j] == target) { // ハッシュ値が条件を満たす
-                if (i - R + 1 >= 0 && j - C + 1 >= 0) { // patternがtext内に収まるとき
+            if (t[i][j] == target) {
+                if (i - R + 1 >= 0 && j - C + 1 >= 0)
                     printf("%d %d\n", i - R + 1, j - C + 1);
-                    // cout << i - R + 1 << " " << j - C + 1 << endl;
-                }
             }
         }
     }
--- aoj/alds114d.cpp
+++ aoj/alds114d.cpp
@@ -1,17 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-#define rep(i, n) for (ll i = 0; i < (ll)n; ++i)
+#define rep(i, n) for (int i = 0; i < n; i++)
 static const int dx[4] = { 0, 1, 0, -1 };
 static const int dy[4] = { 1, 0, -1, 0 };
 static const char dir[4] = { 'u', 'r', 'd', 'l' };
 static const ll INF = 1 << 21;
 static const ll MOD = 1e9 + 7;
+class SuffixArray {
+private:
+    int n;
+    vector<int> rank;
+public:
+    string s;
+    vector<int> idx;
+    SuffixArray(string& s)
+        : n(s.size())
+        , rank(s.size() + 1)
+        , s(s)
+        , idx(s.size() + 1)
+    {
+        rep(i, n + 1)
+        {
+            idx[i] = i;
+            rank[i] = i < n ? s[i] : -1;
+        }
+        int k;
+        auto comp = [&](const int i, const int j) -> bool {
+            if (rank[i] != rank[j]) {
+                return rank[i] < rank[j];
+            } else {
+                int ri = i + k <= n ? rank[i + k] : -1;
+                int rj = j + k <= n ? rank[j + k] : -1;
+                return ri < rj;
+            }
+        };
+
+        vector<int> tmp(n + 1);
+        for (k = 1; k <= n; k *= 2) {
+            sort(idx.begin(), idx.end(), comp);
+
+            tmp[idx[0]] = 0;
+            for (int i = 1; i < n + 1; i++) {
+                tmp[idx[i]] = tmp[idx[i - 1]] + (comp(idx[i - 1], idx[i]) ? 1 : 0);
+            }
+            rank = tmp;
+        }
+    }
+    bool contain(string& t)
+    {
+        int right = n, left = 0;
+        while (right - left > 1) {
+            int mid = (right + left) / 2;
+            if (s.substr(idx[mid], t.size()) < t) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return s.substr(idx[right], t.size()) == t;
+    }
+};
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
+    string T;
+    int Q;
+    cin >> T >> Q;
 
+    SuffixArray suf(T);
+    rep(i, Q)
+    {
+        string P;
+        cin >> P;
+        cout << suf.contain(P) << endl;
+    }
     return 0;
 }
