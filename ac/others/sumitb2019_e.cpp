1558752d361bd1132a74c0ddded4cbc51cc014c4
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2019-12-03 13:35:17 +0900

    12/3

---
+++ ac/others/sumitb2019_e.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)n; ++i)
+static const int dx[4] = { 0, 1, 0, -1 };
+static const int dy[4] = { 1, 0, -1, 0 };
+static const char dir[4] = { 'u', 'r', 'd', 'l' };
+static const ll INF = 1 << 21;
+static const ll MOD = 1e9 + 7;
+
+vector<map<int, int>> vmp;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    rep(i, N) { cin >> A[i]; }
+
+    vmp.resize(3);
+
+
+    return 0;
+}
