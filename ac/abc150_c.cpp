7f57e7d4ccccec51ba45936d31ed326d7bf52abb
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2020-01-11 17:39:28 +0900

    1/11

--- ac/abc150_c.cpp
+++ ac/abc150_c.cpp
@@ -12,8 +12,25 @@ static const ll MOD = 1e9 + 7;
 signed main() {
   int N;
   cin >> N;
-  vector<int> P(N), Q(N);
+  vector<int> P(N), Q(N), A(N);
   rep(i, N) { cin >> P[i]; }
   rep(i, N) { cin >> Q[i]; }
+  rep(i, N) { A[i] = i + 1; }
+  int p, q, num = 0;
+  do {
+    int cntp = 0, cntq = 0;
+    rep(i, N) {
+      if (A[i] == P[i]) cntp++;
+      if (A[i] == Q[i]) cntq++;
+    }
+    if (cntp == N) {
+      p = num;
+    }
+    if (cntq == N) {
+      q = num;
+    }
+    num++;
+  } while (next_permutation(all(A)));
+  cout << abs(p - q) << endl;
   return 0;
 }
--- ac/abc150_d.cpp
+++ ac/abc150_d.cpp
@@ -8,8 +8,27 @@ static const int dy[4] = {1, 0, -1, 0};
 static const char dir[4] = {'u', 'r', 'd', 'l'};
 static const ll INF = 1 << 21;
 static const ll MOD = 1e9 + 7;
+int N, M;
 
 signed main() {
+  cin >> N >> M;
+  vector<ll> a(N);
+  rep(i, N) {
+    cin >> a[i];
+    a[i] /= 2;
+  }
+
+  ll L = 1;  // 最小公倍数
+  rep(i, N) { L = L * a[i] / __gcd(L, a[i]); }
+
+  rep(i, N) {
+    if (L / a[i] % 2 == 0) {
+      std::cout << 0 << endl;
+      return 0;
+    }
+  }
+
+  std::cout << (M + L) / L / 2 << endl;
 
   return 0;
 }
