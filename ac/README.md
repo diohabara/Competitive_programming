38d88c440ac2e5433c2e6a9f6580b2d434a12b2c
diohabara <diohabara@gmail.com>
2021-03-22 14:03:33 +0900

    feat: make new template

---
+++ ac/README.md
@@ -0,0 +1,20 @@
+# AtCoder
+
+## deps
+
+- [`atcoder-tools`](https://github.com/kyuridenamida/atcoder-tools)
+- [`vscode`](https://github.com/microsoft/vscode)
+
+## Usage
+
+### CLI
+
+```sh
+atcoder-tools gen { contest-id }
+```
+
+### VSCode
+
+- `ctrl + c`: compile and run
+- `ctrl + t`: test
+- `ctrl + s`: submit
---
+++ ac/my_template.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const long long mod = 1e9 + 7;
+using ll = long long;
+
+#define INF (1e9)
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+{% if mod %}
+const long long MOD = {{ mod }};
+{% endif %}
+{% if yes_str %}
+const string YES = "{{ yes_str }}";
+{% endif %}
+{% if no_str %}
+const string NO = "{{ no_str }}";
+{% endif %}
+
+{% if prediction_success %}
+void solve({{ formal_arguments }}){
+
+}
+{% endif %}
+
+int main(){
+    // cout << fixed << setprecision(5);
+
+    {% if prediction_success %}
+    {{input_part}}
+    solve({{ actual_arguments }});
+    {% else %}
+    // Failed to predict input format
+    {% endif %}
+    return 0;
+}
