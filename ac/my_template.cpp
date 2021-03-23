00c72b20766205ff00c1764d53635ebf883b6ac2
diohabara <diohabara@gmail.com>
2021-03-23 10:13:35 +0900

    refactor: the make template file plainer

--- ac/my_template.cpp
+++ ac/my_template.cpp
@@ -15,20 +15,6 @@ const string YES = "{{ yes_str }}";
 const string NO = "{{ no_str }}";
 {% endif %}
 
-{% if prediction_success %}
-void solve({{ formal_arguments }}){
-
-}
-{% endif %}
-
 int main(){
-    // cout << fixed << setprecision(5);
-
-    {% if prediction_success %}
-    {{input_part}}
-    solve({{ actual_arguments }});
-    {% else %}
-    // Failed to predict input format
-    {% endif %}
     return 0;
 }
