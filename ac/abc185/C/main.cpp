0a20fd5a37e07b6c6362d998ca2695cea949b481
diohabara <diohabara@gmail.com>
2021-04-18 22:07:55 +0900

    fix: review

--- ac/abc185/C/main.cpp
+++ ac/abc185/C/main.cpp
@@ -7,17 +7,6 @@ const ll MOD = 1e9 + 7;
 const int INF = INT_MAX;
 const ll LLINF = LLONG_MAX;
 
-ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
-ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
-
-ll factorial(ll n) {
-  ll res = 1;
-  for (int i = 1; i <= n; ++i) {
-    res *= i;
-  }
-  return res;
-}
-
 int main() {
   // input
   ll L;
@@ -25,8 +14,8 @@ int main() {
 
   // solve
   ll res = 1;
-  for (int i= 1; i<= 11; ++i) {
-    res *= (L-i);
+  for (ll i = 1; i <= 11; i++) {
+    res *= (L - i);
     res /= i;
   }
   cout << res << endl;
