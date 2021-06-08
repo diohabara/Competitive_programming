51125bd1c572a1bf5627034713cc3ab80a303bbf
diohabara <diohabara@gmail.com>
2021-06-08 16:06:40 +0900

    feat: enable multi-precision number

--- ac/template/my_template.cpp
+++ ac/template/my_template.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp>
 using namespace std;
+using namespace boost::multiprecision;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
@@ -7,12 +9,11 @@ const ll MOD = 1e9 + 7;
 const int INF = INT_MAX;
 const ll LLINF = LLONG_MAX;
 
-{%if yes_str %}
+{ % if yes_str % }
 const string YES = "{{ yes_str }}";
-{% endif %}
-{% if no_str %}
+{ % endif % } { % if no_str % }
 const string NO = "{{ no_str }}";
-{% endif %}
+{ % endif % }
 
 int main() {
   // input
