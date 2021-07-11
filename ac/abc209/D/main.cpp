0fb133070ea598e5b0a3e4a625322a20ef633e35
diohabara <diohabara@gmail.com>
2021-07-11 10:30:59 +0900

    feat: review

--- ac/abc209/D/main.cpp
+++ ac/abc209/D/main.cpp
@@ -12,31 +12,42 @@ const ll MOD = 1e9 + 7;
 const int INF = INT_MAX;
 const ll LLINF = LLONG_MAX;
 
-struct Edge {
-  int to;
-  ll w;
-  Edge(int to, ll w) : to(to), w(w) {}
-};
-
-using Graph = vector<vector<Edge>>;
-
-template <class T>
-bool chmin(T& a, T b) {
-  if (a > b) {
-    a = b;
-    return true;
-  }
-  return false;
-}
-
 int main() {
   // input
   int N, Q;
   cin >> N >> Q;
   vector<ll> a(N), b(N);
   vector<ll> c(Q), d(Q);
-  rep(i, N) { cin >> a[i] >> b[i]; }
+  rep(i, N - 1) { cin >> a[i] >> b[i]; }
   rep(i, Q) { cin >> c[i] >> d[i]; }
 
   // solve
+  vector<vector<int>> G(N);
+  rep(i, N - 1) {
+    a[i]--, b[i]--;
+    G[a[i]].push_back(b[i]);
+    G[b[i]].push_back(a[i]);
+  }
+  queue<int> que;
+  vector<int> dist(N, -1);
+  que.push(0);
+  dist[0] = 0;
+  while (!que.empty()) {
+    int cur = que.front();
+    que.pop();
+    for (int x : G[cur]) {
+      if (dist[x] == -1) {
+        dist[x] = dist[cur] + 1;
+        que.push(x);
+      }
+    }
+  }
+  rep(i, Q) {
+    c[i]--, d[i]--;
+    if (dist[c[i]] % 2 == dist[d[i]] % 2) {
+      cout << "Town" << endl;
+    } else {
+      cout << "Road" << endl;
+    }
+  }
 }
