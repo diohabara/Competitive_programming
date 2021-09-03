7a85b827ca6fd40b8e606d682a2a9346c18edb49
diohabara <diohabara@gmail.com>
2021-09-03 22:40:06 +0900

    fix: review abc208

--- ac/abc208/D/main.cpp
+++ ac/abc208/D/main.cpp
@@ -17,14 +17,28 @@ int main() {
   ll N, M;
   cin >> N >> M;
   vector<ll> A(M), B(M), C(M);
-  rep(i, M) { cin >> A[i] >> B[i] >> C[i]; }
+  rep(i, M) {
+    cin >> A[i] >> B[i] >> C[i];
+    A[i]--;
+    B[i]--;
+  }
 
   // solve
-  vector<ll> dist(N);
+  vector<vector<ll>> dist(N, vector<ll>(N, INF));
   ll res = 0;
-  rep(i, M) {
-    rep(j, M) {
-      rep(k, M) {}
+  rep(i, N) { dist[i][i] = 0; }
+  rep(i, M) { dist[A[i]][B[i]] = C[i]; }
+  rep(k, N) {
+    rep(s, N) {
+      rep(t, N) { dist[s][t] = min(dist[s][t], dist[s][k] + dist[k][t]); }
+    }
+    rep(s, N) {
+      rep(t, N) {
+        if (dist[s][t] < INF) {
+          res += dist[s][t];
+        }
+      }
     }
   }
+  cout << res << endl;
 }
