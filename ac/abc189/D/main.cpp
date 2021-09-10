b2772a1295da8fb464809caf61855a52ff00a0c1
diohabara <diohabara@gmail.com>
2021-09-10 10:11:12 +0900

    feat: abc 189 D

--- ac/abc189/D/main.cpp
+++ ac/abc189/D/main.cpp
@@ -9,4 +9,22 @@ const ll INF = 1e18;
 ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
 ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
 
-int main() { return 0; }
+int main() {
+  // input
+  int N;
+  cin >> N;
+  vector<string> S(N);
+  for (string& s : S) {
+    cin >> s;
+  }
+
+  // solve
+  ll ans = 1;  // i == 0
+  rep(i, N) {
+    if (S[i] == "OR") {
+      ans += (1 << (i + 1));
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
