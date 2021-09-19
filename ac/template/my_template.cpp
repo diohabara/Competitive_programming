9605a4e392f2a4abd9619192e4120c081eaa5ef3
diohabara <diohabara@gmail.com>
2021-09-19 19:02:20 +0900

    feat: change config

--- ac/template/my_template.cpp
+++ ac/template/my_template.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-// #include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-// using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
