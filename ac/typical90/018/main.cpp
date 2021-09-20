25e86a8ef2888dc72de0e1cfeb5dc03026180835
diohabara <diohabara@gmail.com>
2021-09-20 21:25:26 +0900

    fix: typical 018

--- ac/typical90/018/main.cpp
+++ ac/typical90/018/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -14,6 +12,25 @@ const ll LLINF = LLONG_MAX;
 
 int main() {
   // input
+  double T;
+  cin >> T;
+  double L, X, Y;
+  cin >> L >> X >> Y;
+  int Q;
+  cin >> Q;
+  vector<double> E(Q);
+  rep(i, Q) { cin >> E[i]; }
+  cout << setprecision(15);
 
   // solve
+  rep(i, Q) {
+    long double theta = E[i] / T * 2.0 * M_PI;
+    long double cx = 0;
+    long double cy = -(L / 2.0) * sin(theta);
+    long double cz = (L / 2.0) - (L / 2.0) * cos(theta);
+    long double d1 = sqrt((cx - X) * (cx - X) + (cy - Y) * (cy - Y));
+    long double d2 = cz;
+    long double angle = atan2(d2, d1);
+    cout << angle * 180.0 / M_PI << endl;
+  }
 }
