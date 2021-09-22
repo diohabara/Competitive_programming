759f1b2c9432636e1a090b40e184a5a318380af7
diohabara <diohabara@gmail.com>
2021-09-22 13:57:46 +0900

    fix: typical 038

--- ac/typical90/038/main.cpp
+++ ac/typical90/038/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -14,6 +12,18 @@ const ll LLINF = LLONG_MAX;
 
 int main() {
   // input
+  ll A, B;
+  cin >> A >> B;
+  const ll POW18 = 1000000000000000000LL;
 
   // solve
+  ll r = B / gcd(A, B);
+  /** necessary condtion to LCM(= A*B/GCD) is over 1e18
+   * POW18 * GCD < A * B
+   */
+  if (POW18 / A < r) {
+    cout << "Large" << endl;
+  } else {
+    cout << A * r << endl;
+  }
 }
