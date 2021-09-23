bc45535ecbb39d16668b27882b5033b4bcf909e0
diohabara <diohabara@gmail.com>
2021-09-23 16:46:03 +0900

    fix: typical 048 050

--- ac/typical90/048/main.cpp
+++ ac/typical90/048/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -14,6 +12,19 @@ const ll LLINF = LLONG_MAX;
 
 int main() {
   // input
+  int N, K;
+  cin >> N >> K;
+  vector<ll> arr;
+  rep(i, N) {
+    ll a, b;
+    cin >> a >> b;
+    arr.push_back(b);
+    arr.push_back(a - b);
+  }
 
   // solve
+  sort(arr.begin(), arr.end(), greater<ll>());
+  ll ans = 0;
+  rep(i, K) { ans += arr[i]; }
+  cout << ans << endl;
 }
--- ac/typical90/050/main.cpp
+++ ac/typical90/050/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -14,6 +12,19 @@ const ll LLINF = LLONG_MAX;
 
 int main() {
   // input
+  int N, L;
+  cin >> N >> L;
 
   // solve
+  vector<ll> res(N + 1);
+  res[0] = 1;
+  rep(i, N) {
+    if (i + L <= N) {
+      res[i + L] += res[i];
+      res[i + L] %= MOD;
+    }
+    res[i + 1] += res[i];
+    res[i + 1] %= MOD;
+  }
+  cout << res[N] << endl;
 }
