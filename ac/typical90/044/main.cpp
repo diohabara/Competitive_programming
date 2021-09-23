be6b3b0d357ba7f6448063b57f1f53f30d06a988
diohabara <diohabara@gmail.com>
2021-09-23 11:10:48 +0900

    fix: review 044 046

--- ac/typical90/044/main.cpp
+++ ac/typical90/044/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -14,6 +12,23 @@ const ll LLINF = LLONG_MAX;
 
 int main() {
   // input
+  int N, Q;
+  cin >> N >> Q;
+  vector<int> A(N);
+  rep(i, N) { cin >> A[i]; }
 
   // solve
+  int shift_cnt = 0;
+  rep(_, Q) {
+    int T, x, y;
+    cin >> T >> x >> y;
+    x--, y--;
+    if (T == 1) {
+      swap(A[(x + shift_cnt) % N], A[(y + shift_cnt) % N]);
+    } else if (T == 2) {
+      shift_cnt = (shift_cnt + N - 1) % N;
+    } else {
+      cout << A[(x + shift_cnt) % N] << endl;
+    }
+  }
 }
--- ac/typical90/046/main.cpp
+++ ac/typical90/046/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -14,6 +12,29 @@ const ll LLINF = LLONG_MAX;
 
 int main() {
   // input
+  int N;
+  cin >> N;
+  vector<int> A(N), B(N), C(N);
+  rep(i, N) cin >> A[i];
+  rep(i, N) cin >> B[i];
+  rep(i, N) cin >> C[i];
 
   // solve
+  vector<ll> cnt_of_a(46), cnt_of_b(46), cnt_of_c(46);
+  rep(i, N) {
+    cnt_of_a[A[i] % 46]++;
+    cnt_of_b[B[i] % 46]++;
+    cnt_of_c[C[i] % 46]++;
+  }
+  ll res = 0;
+  rep(i, 46) {
+    rep(j, 46) {
+      rep(k, 46) {
+        if ((i + j + k) % 46 == 0) {
+          res += cnt_of_a[i] * cnt_of_b[j] * cnt_of_c[k];
+        }
+      }
+    }
+  }
+  cout << res << endl;
 }
