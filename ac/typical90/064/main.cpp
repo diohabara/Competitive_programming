8d531fb4516423a20919a1ce3682e99a65872500
diohabara <diohabara@gmail.com>
2021-09-26 18:18:44 +0900

    fix: typical 064 067

--- ac/typical90/064/main.cpp
+++ ac/typical90/064/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -14,6 +12,51 @@ const ll LLINF = LLONG_MAX;
 
 int main() {
   // input
+  int N, Q;
+  cin >> N >> Q;
+  vector<ll> A(N);
+  rep(i, N) { cin >> A[i]; }
+  vector<ll> L(Q), R(Q), V(Q);
+  rep(i, Q) {
+    cin >> L[i] >> R[i] >> V[i];
+    R[i]--, L[i]--;
+  }
 
   // solve
+  ll conv = 0;          // sum of "incovenience"
+  vector<ll> B(N - 1);  // sequence of differences
+  rep(i, N - 1) {
+    B[i] = A[i + 1] - A[i];
+    conv += abs(B[i]);
+  }
+
+  rep(i, Q) {
+    /**
+     * A := [left] + [changed] + [right]
+     * [rightmost of left] and [leftmost of right] changes the convenience.
+     * L[i] = 1, R[i] = 2, V[i] = 3
+     * A: [1, 2, 6, 4, 9]
+     * => [4, 5, 6, 4, 9]
+     * B: [1, 4, 2, 5]
+     * => [1, 1, 2, 5]
+     *
+     * L[i] = 2, R[i] = 4, V[i] = -2
+     * A: [1, 2, 6, 4, 9]
+     * => [1, 0, 4, 2, 9]
+     * B: [1, 4, 2, 5]
+     * => [1, 4, 2, 7]
+     */
+    int l = L[i], r = R[i], v = V[i];
+    if (0 < l) {
+      conv -= abs(B[l - 1]);  // convenience before
+      B[l - 1] += v;
+      conv += abs(B[l - 1]);  // convenience after
+    }
+    if (r < N - 1) {
+      conv -= abs(B[r]);
+      B[r] -= v;
+      conv += abs(B[r]);
+    }
+    cout << conv << endl;
+  }
 }
--- ac/typical90/067/main.cpp
+++ ac/typical90/067/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -12,8 +10,34 @@ const ll MOD = 1e9 + 7;
 const int INF = INT_MAX;
 const ll LLINF = LLONG_MAX;
 
+ll base8_to_int(string N) {
+  ll res = 0;
+  rep(i, N.size()) { res = res * 8 + int(N[i] - '0'); }
+  return res;
+}
+
+string int_to_base9(ll N) {
+  if (N == 0) {
+    return "0";
+  }
+  string res;
+  while (0 < N) {
+    res = char(N % 9 + '0') + res;
+    N /= 9;
+  }
+  return res;
+}
+
 int main() {
   // input
+  string N;
+  int K;
+  cin >> N >> K;
 
   // solve
+  rep(_, K) {
+    N = int_to_base9(base8_to_int(N));
+    replace(N.begin(), N.end(), '8', '5');
+  }
+  cout << N << endl;
 }
