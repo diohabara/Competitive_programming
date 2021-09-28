03381c9cd4fdd4d81d8233963546cd4f9d51711c
diohabara <diohabara@gmail.com>
2021-09-28 09:32:16 +0900

    fix: abc218 e

--- ac/abc218/E/main.cpp
+++ ac/abc218/E/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -16,15 +14,28 @@ int main() {
   // input
   int N, M;
   cin >> N >> M;
-  vector<int> edge(N);
-  vector<int> cost(M);
+  vector<tuple<int, int, int>> edges(M);
   rep(i, M) {
-    int a, b;
-    cin >> a >> b;
+    ll a, b, c;
+    cin >> a >> b >> c;
     a--, b--;
-    edge[a].push_back(b);
-    edge[b].push_back(a);
-    cin >> cost[i];
+    edges[i] = make_tuple(a, b, c);
   }
-  return 0;
+
+  // solve
+  sort(edges.begin(), edges.end(),
+       [](auto& lhs, auto& rhs) { return get<2>(lhs) < get<2>(rhs); });
+  dsu d(N);
+  ll res = 0;
+  for (auto [a, b, c] : edges) {
+    /** choose unconnected node
+     * or cost is minus
+     */
+    if (d.leader(a) != d.leader(b) || c < 0) {
+      d.merge(a, b);
+    } else {
+      res += c;
+    }
+  }
+  cout << res << endl;
 }
