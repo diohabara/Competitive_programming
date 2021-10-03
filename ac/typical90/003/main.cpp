ae555ff19427c44a015dc65aba2264a0b88f319b
diohabara <diohabara@gmail.com>
2021-10-03 22:55:12 +0900

    refactor: more fixable

--- ac/typical90/003/main.cpp
+++ ac/typical90/003/main.cpp
@@ -10,22 +10,30 @@ const ll MOD = 1e9 + 7;
 const int INF = INT_MAX;
 const ll LLINF = LLONG_MAX;
 
-void get_dist(int start, int N, vector<ll>& dist, vector<vector<ll>>& G) {
-  rep(i, N) { dist[i] = INF; }
-
-  queue<int> Q;
-  Q.push(start);
-  dist[start] = 0;
-  while (!Q.empty()) {
-    int pos = Q.front();
+/**
+ * return: <distance, index>
+ * return furthest node with distance
+ */
+pair<ll, ll> get_furthest_node(const vector<vector<ll>> graph, const ll from,
+                               const ll parent) {
+  queue<ll> Q;
+  Q.push(from);
+  vector<ll> dist(graph.size(), INF);
+  dist[from] = 0;
+  int furthest_node;
+  while (Q.size()) {
+    ll now = Q.front();
     Q.pop();
-    for (auto& to : G[pos]) {
-      if (dist[to] == INF) {
-        dist[to] = dist[pos] + 1;
-        Q.push(to);
+    furthest_node = now;
+    for (ll next : graph[now]) {
+      if (next == parent) continue;
+      if (dist[next] == INF) {
+        dist[next] = dist[now] + 1;
+        Q.push(next);
       }
     }
   }
+  return make_pair(dist[furthest_node], furthest_node);
 }
 
 int main() {
@@ -36,25 +44,13 @@ int main() {
   rep(_, N - 1) {
     int a, b;
     cin >> a >> b;
-    a--;
-    b--;
+    a--, b--;
     graph[a].push_back(b);
     graph[b].push_back(a);
   }
 
   // solve
-  vector<ll> dist(N);
-  get_dist(0, N, dist, graph);
-  int maxn1 = -1, maxid1 = -1;
-  rep(i, N) {
-    if (maxn1 < dist[i]) {
-      maxn1 = dist[i];
-      maxid1 = i;
-    }
-  }
-
-  get_dist(maxid1, N, dist, graph);
-  ll maxn2 = 1;
-  rep(i, N) { maxn2 = max(maxn2, dist[i]); }
-  cout << maxn2 + 1 << endl;
+  auto p = get_furthest_node(graph, 0, -1);
+  auto q = get_furthest_node(graph, p.second, -1);
+  cout << q.first + 1 << endl;
 }
