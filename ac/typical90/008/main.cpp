288b4f1002ca9cb589bbbf22968241e02229d0d0
diohabara <diohabara@gmail.com>
2021-10-04 23:19:02 +0900

    fix: typical 008

--- ac/typical90/008/main.cpp
+++ ac/typical90/008/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -14,6 +12,24 @@ const ll LLINF = LLONG_MAX;
 
 int main() {
   // input
+  int N;
+  cin >> N;
+  string S;
+  cin >> S;
 
   // solve
+  // dp[pos][i] := pos-th string has up to the i-th character of "atcoder"
+  vector<vector<ll>> dp(N + 1, vector<ll>(8));
+  dp[0][0] = 1;
+  string atcoder = "atcoder_";
+  rep(i, N) {
+    rep(j, atcoder.size()) {
+      dp[i + 1][j] += dp[i][j];
+      if (S[i] == atcoder[j]) {
+        dp[i + 1][j + 1] += dp[i][j];
+      }
+    }
+    rep(j, atcoder.size()) { dp[i + 1][j] %= MOD; }
+  }
+  cout << dp[N][7] << endl;
 }
