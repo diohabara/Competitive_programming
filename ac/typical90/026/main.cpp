b432721b356f953794517240df37c6fae98f6fd5
diohabara <diohabara@gmail.com>
2021-10-06 19:46:01 +0900

    fix: typical 026 028

--- ac/typical90/026/main.cpp
+++ ac/typical90/026/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -12,8 +10,52 @@ const ll MOD = 1e9 + 7;
 const int INF = INT_MAX;
 const ll LLINF = LLONG_MAX;
 
+void dfs(vector<vector<int>>& graph, vector<int>& color, int current_node,
+         int current_color) {
+  color[current_node] = current_color;
+  for (auto& next_node : graph[current_node]) {
+    if (color[next_node] != -1) continue;
+    dfs(graph, color, next_node, current_color ^ 1);
+  }
+}
+
 int main() {
   // input
+  int N;
+  cin >> N;
+  vector<vector<int>> graph(N);
+  rep(i, N - 1) {
+    int a, b;
+    cin >> a >> b;
+    a--, b--;
+    graph[a].push_back(b);
+    graph[b].push_back(a);
+  }
 
   // solve
+  vector<int> v1, v2;
+  vector<int> color(N, -1);
+  // bipartite graph coloring
+  dfs(graph, color, 0, 0);
+  // choose vertices by color
+  rep(i, N) {
+    if (color[i] == 0) {
+      v1.push_back(i);
+    } else {
+      v2.push_back(i);
+    }
+  }
+  // output
+  if (v1.size() < v2.size()) {
+    rep(i, N / 2) {
+      if (i) cout << " ";
+      cout << v2[i] + 1;
+    }
+  } else {
+    rep(i, N / 2) {
+      if (i) cout << " ";
+      cout << v1[i] + 1;
+    }
+  }
+  cout << endl;
 }
--- ac/typical90/028/main.cpp
+++ ac/typical90/028/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -14,6 +12,34 @@ const ll LLINF = LLONG_MAX;
 
 int main() {
   // input
+  int N;
+  cin >> N;
+  vector<vector<int>> paper(1001, vector<int>(1001));
+  rep(i, N) {
+    int lx, ly, rx, ry;
+    cin >> lx >> ly >> rx >> ry;
+    paper[lx][ly]++;
+    paper[rx][ry]++;
+    paper[lx][ry]--;
+    paper[rx][ly]--;
+  }
 
   // solve
+  // horizontally cumulative sum
+  rep(y, 1001) {
+    rep(x, 1000) { paper[y][x + 1] += paper[y][x]; }
+  }
+  // vertically cumulative sum
+  rep(x, 1001) {
+    rep(y, 1000) { paper[y + 1][x] += paper[y][x]; }
+  }
+  vector<ll> ans(N + 1);
+  for (auto& row : paper) {
+    for (auto& e : row) {
+      ans[e]++;
+    }
+  }
+  for (int i = 1; i <= N; i++) {
+    cout << ans[i] << endl;
+  }
 }
