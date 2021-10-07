b497a4d4f6fc318fcf6cb29e334306a313a18689
diohabara <diohabara@gmail.com>
2021-10-08 08:09:00 +0900

    fix: typical 058

--- ac/typical90/058/main.cpp
+++ ac/typical90/058/main.cpp
@@ -1,19 +1,52 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
-const ll MOD = 1e9 + 7;
+const ll MOD = 100000;
 const int INF = INT_MAX;
 const ll LLINF = LLONG_MAX;
 
+ll digit_sum(ll n) {
+  ll sum = 0;
+  while (0 < n) {
+    sum += n % 10;
+    n /= 10;
+  }
+  return sum;
+}
+
 int main() {
   // input
+  ll N, K;
+  cin >> N >> K;
 
   // solve
+  vector<int> next(MOD);  // next[i] := i's next number
+  rep(i, MOD) next[i] = (i + digit_sum(i)) % MOD;
+
+  vector<int> timestamp(MOD,
+                        -1);  // timestamp[i] := the first time that i arrives
+  int cur_n = N, cnt = 0;
+  //  make timestamps
+  while (timestamp[cur_n] == -1) {
+    timestamp[cur_n] = cnt;
+    cur_n = next[cur_n];
+    cnt++;
+  }
+  ll cycle = cnt - timestamp[cur_n];  // steps it takes to get back to N
+  if (timestamp[cur_n] <= K) {
+    K = (K - timestamp[cur_n] + cycle) % cycle;  // periodic part
+    K += timestamp[cur_n];                       // remainder part
+  }
+  ll ans = -1;
+  rep(i, MOD) {
+    if (timestamp[i] == K) {
+      ans = i;
+    }
+  }
+  cout << ans << endl;
 }
