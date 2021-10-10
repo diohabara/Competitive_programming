fe99e15291446eb527a30967b98242eb9fe588e9
diohabara <diohabara@gmail.com>
2021-10-10 20:50:18 +0900

    fix: typical 063 070

--- ac/typical90/063/main.cpp
+++ ac/typical90/063/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -14,6 +12,39 @@ const ll LLINF = LLONG_MAX;
 
 int main() {
   // input
+  int H, W;
+  cin >> H >> W;
+  vector<vector<int>> P(H, vector<int>(W));
+  rep(i, H) rep(j, W) cin >> P[i][j];
 
   // solve
+  int ans = 0;
+  // brute-force seach rows using bit-mask
+  for (int mask = 1; mask < (1 << H); mask++) {
+    vector<int> R;
+    map<int, int> cnt_of_num;
+    // find the longest continuous sequence of same numbers
+    // for each column, every element must be the same
+    rep(col, W) {
+      int num = -1;
+      bool is_all_same = true;
+      rep(row, H) {
+        if ((mask & (1 << row)) == 0) continue;
+        if (num == -1)
+          num = P[row][col];
+        else if (num != P[row][col])
+          is_all_same = false;
+      }
+      if (is_all_same) cnt_of_num[num]++;
+    }
+    int cntH = 0, cntW = 0;
+    for (auto& [num, cnt] : cnt_of_num) {
+      cntW = max(cntW, cnt);
+    }
+    rep(row, H) {
+      if (mask & (1 << row)) cntH++;
+    }
+    ans = max(ans, cntH * cntW);
+  }
+  cout << ans << endl;
 }
--- ac/typical90/070/main.cpp
+++ ac/typical90/070/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -14,6 +12,20 @@ const ll LLINF = LLONG_MAX;
 
 int main() {
   // input
+  int N;
+  cin >> N;
+  // vector<pair<int, int>> points(N);
+  // for (auto& [x, y] : points) {
+  //   cin >> x >> y;
+  // }
+  vector<ll> X(N), Y(N);
+  rep(i, N) { cin >> X[i] >> Y[i]; }
 
   // solve
+  sort(X.begin(), X.end());
+  sort(Y.begin(), Y.end());
+  ll res = 0;
+  ll nx = X[N / 2], ny = Y[N / 2];
+  rep(i, N) { res += abs(X[i] - nx) + abs(Y[i] - ny); }
+  cout << res << endl;
 }
