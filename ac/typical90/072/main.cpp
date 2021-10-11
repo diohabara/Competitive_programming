9760e4fe79176eb5b0e70eae1afbd3633d16944c
diohabara <diohabara@gmail.com>
2021-10-11 16:23:41 +0900

    feat: solve all star4 in typical

--- ac/typical90/072/main.cpp
+++ ac/typical90/072/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -12,8 +10,41 @@ const ll MOD = 1e9 + 7;
 const int INF = INT_MAX;
 const ll LLINF = LLONG_MAX;
 
+int H, W;
+vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+vector<vector<bool>> visited;
+
+int dfs(int sx, int sy, int px, int py, vector<string>& c) {
+  // it gets back to the start
+  if (sx == px && sy == py && visited[px][py]) return 0;
+  visited[px][py] = true;  // it visits (px, py)
+  int ret = -INF;          // MUST be small enough
+  for (auto& [dx, dy] : dir) {
+    auto [nx, ny] = make_tuple(px + dx, py + dy);
+    if (nx < 0 || ny < 0 || H <= nx || W <= ny || c[nx][ny] == '#') continue;
+    if ((sx != nx || sy != ny) && visited[nx][ny]) continue;
+    ret = max(ret, dfs(sx, sy, nx, ny, c) + 1);
+  }
+  visited[px][py] = false;  // it backtracks from (px, py)
+  return ret;
+}
+
 int main() {
   // input
+  cin >> H >> W;
+  vector<string> c(H);
+  rep(i, H) { cin >> c[i]; }
 
   // solve
+  int ans = -1;
+  visited.resize(H, vector<bool>(W));
+  rep(x, H) {
+    rep(y, W) {
+      if (c[x][y] == '#') continue;
+      ans = max(ans, dfs(x, y, x, y, c));
+    }
+  }
+
+  if (ans <= 2) ans = -1;
+  cout << ans << endl;
 }
--- ac/typical90/085/main.cpp
+++ ac/typical90/085/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -12,8 +10,35 @@ const ll MOD = 1e9 + 7;
 const int INF = INT_MAX;
 const ll LLINF = LLONG_MAX;
 
+vector<ll> get_divisors(ll n) {
+  vector<ll> divisors;
+  for (ll i = 1; i * i <= n; i++) {
+    if (n % i != 0) continue;
+    divisors.push_back(i);
+    if (i != n / i) divisors.push_back(n / i);
+  }
+  return divisors;
+}
+
 int main() {
   // input
+  ll K;
+  cin >> K;
 
   // solve
+  ll ans = 0;
+
+  auto divisors = get_divisors(K);
+  sort(divisors.begin(), divisors.end());
+
+  rep(i, divisors.size()) {
+    for (int j = i; j < (int)divisors.size(); j++) {
+      ll a = divisors[i], b = divisors[j];
+      ll c = K / a / b;
+      ll rem = K % (a * b);
+      if (a > c || b > c || rem != 0) continue;
+      ans++;
+    }
+  }
+  cout << ans << endl;
 }
