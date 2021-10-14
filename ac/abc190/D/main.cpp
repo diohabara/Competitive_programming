ca3bece3fd5936826dd694c11c8a5424ad851a04
diohabara <diohabara@gmail.com>
2021-10-14 17:41:56 +0900

    fix: abc 189 d

--- ac/abc190/D/main.cpp
+++ ac/abc190/D/main.cpp
@@ -9,4 +9,23 @@ const ll INF = 1e18;
 ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
 ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
 
-int main() { return 0; }
+int main() {
+  // input
+  ll N;
+  cin >> N;
+
+  // solve
+  while (N % 2 == 0) {  // extract all even numbers
+    N /= 2;
+  }
+  ll ans = 0;
+  for (ll i = 1; i * i <= N; i++) {
+    if (N % i == 0) {
+      ans += 2;  // one has two as factor, the other doesn't
+    }
+    if (i * i == N) {
+      ans--;
+    }
+  }
+  cout << 2 * ans << endl;
+}
