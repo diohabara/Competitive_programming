860598ee8f2995980d800259d8869a260c040753
diohabara <diohabara@gmail.com>
2021-10-15 19:50:00 +0900

    feat: abc 183 185

--- ac/abc183/D/main.cpp
+++ ac/abc183/D/main.cpp
@@ -12,6 +12,23 @@ const string NO = "No";
 
 int main() {
   // input
+  int N, W;
+  cin >> N >> W;
+  vector<ll> liter(210000);
+  rep(_, N) {
+    int s, t, p;
+    cin >> s >> t >> p;
+    liter[s] += p;
+    liter[t] -= p;
+  }
 
   // solve
+  rep(i, liter.size() - 1) { liter[i + 1] += liter[i]; }
+  rep(i, liter.size()) {
+    if (W < liter[i]) {
+      cout << NO << endl;
+      return 0;
+    }
+  }
+  cout << YES << endl;
 }
--- ac/abc185/D/main.cpp
+++ ac/abc185/D/main.cpp
@@ -12,6 +12,31 @@ ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
 
 int main() {
   // input
+  int N, M;
+  cin >> N >> M;
+  vector<int> A(M);
+  rep(i, M) cin >> A[i];
 
   // solve
+  sort(A.begin(), A.end());
+  // add sentinels
+  A.insert(A.begin(), 0);
+  A.push_back(N + 1);
+
+  // find the min white length and make the white length vector
+  vector<int> white_lengths;
+  int min_len = N;
+  rep(i, M + 1) {
+    int cur_len = A[i + 1] - A[i] - 1;
+    if (0 < cur_len) {
+      white_lengths.push_back(cur_len);
+      min_len = min(min_len, cur_len);
+    }
+  }
+  ll ans = 0;
+  rep(i, white_lengths.size()) {
+    // floor(white_len / min_len)
+    ans += (white_lengths[i] + min_len - 1) / min_len;
+  }
+  cout << ans << endl;
 }
