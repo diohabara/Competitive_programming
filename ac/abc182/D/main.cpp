1904515c2ff051e9b8a47bd8a1b4b59ef1602510
diohabara <diohabara@gmail.com>
2021-10-17 22:08:01 +0900

    fix: abc 182 D

--- ac/abc182/D/main.cpp
+++ ac/abc182/D/main.cpp
@@ -9,6 +9,21 @@ const ll LLINF = LLONG_MAX;
 
 int main() {
   // input
+  int N;
+  cin >> N;
+  vector<ll> A(N);
+  rep(i, N) cin >> A[i];
 
   // solve
+  ll ans = 0;
+  ll cumulative_sum = 0;  // the sum until the i-th element
+  ll max_on_route = 0;    // the max value of sums until i-th element
+  ll total_sum = 0;       // total sum until the i-th step
+  rep(i, N) {
+    cumulative_sum += A[i];
+    max_on_route = max(max_on_route, cumulative_sum);
+    ans = max(ans, total_sum + max_on_route);
+    total_sum += cumulative_sum;
+  }
+  cout << ans << endl;
 }
