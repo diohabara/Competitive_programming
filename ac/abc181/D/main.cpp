47c0ebb1ec6eab0f61579cc5c728189727682620
diohabara <diohabara@gmail.com>
2021-10-18 10:58:40 +0900

    fix: abc181 d

--- ac/abc181/D/main.cpp
+++ ac/abc181/D/main.cpp
@@ -12,6 +12,37 @@ const string NO = "No";
 
 int main() {
   // input
+  string S;
+  cin >> S;
 
   // solve
+  if (S.size() == 1 && S == "8") {
+    cout << YES << endl;
+    return 0;
+  } else if (S.size() == 2) {
+    if (stoi(S) % 8 == 0) {
+      cout << YES << endl;
+      return 0;
+    }
+    swap(S[0], S[1]);
+    if (stoi(S) % 8 == 0) {
+      cout << YES << endl;
+      return 0;
+    }
+  }
+  vector<int> cnt(10);               // cnt[i] := cnt of i
+  for (auto& c : S) cnt[c - '0']++;  // count the characters of S
+  for (int multi = 104; multi < 1000; multi += 8) {
+    auto _cnt = cnt;
+    for (auto& c : to_string(multi)) {
+      // decrement the count of characters of multiples of 8
+      _cnt[c - '0']--;
+    }
+    // all character count must be greater or equal to 0
+    if (all_of(_cnt.begin(), _cnt.end(), [](int x) { return x >= 0; })) {
+      cout << YES << endl;
+      return 0;
+    }
+  }
+  cout << NO << endl;
 }
