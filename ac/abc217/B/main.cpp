b8b9fcb6159730c8eed100562bbde462b39b5410
diohabara <diohabara@gmail.com>
2021-10-22 23:29:26 +0900

    fix: abc217 b

--- ac/abc217/B/main.cpp
+++ ac/abc217/B/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -14,6 +12,19 @@ const ll LLINF = LLONG_MAX;
 
 int main() {
   // input
+  set<string> S;
+  rep(_, 3) {
+    string s;
+    cin >> s;
+    S.insert(s);
+  }
 
   // solve
+  vector<string> T{"ABC", "ARC", "AGC", "AHC"};
+  for (auto& s : T) {
+    if (S.find(s) == S.end()) {
+      cout << s << endl;
+      return 0;
+    }
+  }
 }
