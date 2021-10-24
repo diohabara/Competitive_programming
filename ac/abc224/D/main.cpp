af4e66989771c0860207d6cb670eb33bde5532be
diohabara <diohabara@gmail.com>
2021-10-24 11:59:41 +0900

    fix: abc224 d

--- ac/abc224/D/main.cpp
+++ ac/abc224/D/main.cpp
@@ -14,21 +14,41 @@ int main() {
   // input
   int M;
   cin >> M;
-  vector<vector<int>> graph(9);
+  vector<vector<int>> graph(10);
+  int u, v;
   rep(i, M) {
-    int u, v;
     cin >> u >> v;
-    u--, v--;
     graph[u].push_back(v);
     graph[v].push_back(u);
   }
-  vector<bool> p(9);
-  rep(i, 8) {
-    int x;
-    cin >> x;
-    x--;
-    p[x] = true;
+  int p;
+  string s = "999999999";
+  for (int i = 1; i <= 8; i++) {
+    cin >> p;
+    s[p - 1] = i + '0';
   }
 
   // solve
+  queue<string> Q;
+  Q.push(s);
+  map<string, int> mp;
+  mp[s] = 0;
+  while (Q.size()) {
+    string s = Q.front();
+    Q.pop();
+    for (int i = 1; i <= 9; i++)
+      if (s[i - 1] == '9') v = i;
+
+    for (auto& u : graph[v]) {
+      string t = s;
+      swap(t[u - 1], t[v - 1]);
+      if (mp.count(t)) continue;
+      mp[t] = mp[s] + 1;
+      Q.push(t);
+    }
+  }
+  if (mp.count("123456789") == 0)
+    cout << -1 << endl;
+  else
+    cout << mp["123456789"] << endl;
 }
