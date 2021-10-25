aa144c862cb87db3a3534cdd15a69ccc9cadfafc
diohabara <diohabara@gmail.com>
2021-10-25 16:09:03 +0900

    fix: abc217 c

--- ac/abc217/C/main.cpp
+++ ac/abc217/C/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -14,6 +12,22 @@ const ll LLINF = LLONG_MAX;
 
 int main() {
   // input
+  int N;
+  cin >> N;
+  vector<int> p(N);
+  rep(i, N) {
+    cin >> p[i];
+    p[i]--;
+  }
 
   // solve
+  vector<int> Q(N);
+  rep(i, N) { Q[p[i]] = i + 1; }
+  rep(i, N) {
+    if (i) {
+      cout << " ";
+    }
+    cout << Q[i];
+  }
+  cout << endl;
 }
