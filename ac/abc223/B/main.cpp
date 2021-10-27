5a8b1a6b5eafe6ae5f656d44a01e12e3bb6e00f0
diohabara <diohabara@gmail.com>
2021-10-27 13:21:32 +0900

    fix: abc223 b

--- ac/abc223/B/main.cpp
+++ ac/abc223/B/main.cpp
@@ -12,6 +12,16 @@ const ll LLINF = LLONG_MAX;
 
 int main() {
   // input
+  string S;
+  cin >> S;
 
   // solve
+  string ma = S, mi = S;
+  rep(i, S.size()) {
+    rotate(S.begin(), S.begin() + 1, S.end());
+    ma = max(S, ma);
+    mi = min(S, mi);
+  }
+  cout << mi << endl;
+  cout << ma << endl;
 }
