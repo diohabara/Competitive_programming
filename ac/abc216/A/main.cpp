01ddaa53a73571230e53b51340ed3332e152ff14
diohabara <diohabara@gmail.com>
2021-10-28 23:24:47 +0900

    fix: abc216 a

--- ac/abc216/A/main.cpp
+++ ac/abc216/A/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace atcoder;
-using namespace boost::multiprecision;
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -14,6 +12,16 @@ const ll LLINF = LLONG_MAX;
 
 int main() {
   // input
+  string S;
+  cin >> S;
 
   // solve
+  int Y = S.back() - '0';
+  cout << S.substr(0, S.size() - 2);
+  if (Y <= 2) {
+    cout << "-";
+  } else if (7 <= Y && Y <= 9) {
+    cout << "+";
+  }
+  cout << endl;
 }
