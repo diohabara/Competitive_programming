08f5394d6f2643b5ec76cbf6d7b23d2d962622a5
diohabara <diohabara@gmail.com>
2021-10-31 22:03:12 +0900

    fix: abc222 a

---
+++ ac/abc222/A/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace atcoder;
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+const ll MOD = 1e9 + 7;
+const int INF = INT_MAX;
+const ll LLINF = LLONG_MAX;
+
+int main() {
+  // input
+  int N;
+  cin >> N;
+
+  // solve
+  printf("%04d\n", N);
+}
