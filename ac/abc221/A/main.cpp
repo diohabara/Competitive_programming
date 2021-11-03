a326d2331fcca5c15e4b0767e5415029118253df
diohabara <diohabara@gmail.com>
2021-11-03 23:11:35 +0900

    fix: abc221 a

---
+++ ac/abc221/A/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace atcoder;
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+const ll MOD = 1e9 + 7;
+const int INF = INT_MAX;
+const ll LLINF = LLONG_MAX;
+
+int main() {
+  // input
+  int A, B;
+  cin >> A >> B;
+
+  // solve
+  int diff = A - B;
+  cout << (int)pow(32, diff) << endl;
+}
